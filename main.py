341eb168953eb1d095c8c2037974771bc0b9cfad
morgane-du-tweize <morgane_rt@hotmail.com>
2025-01-06 12:13:42 +0100

    ajouté documentation des fonctions

--- main.py
+++ main.py
@@ -1,16 +1,19 @@
 # implémenter le score
 
+# initialise une grille vierge de 3 lignes * 3 colonnes
 def nouvelle_grille() :
     grille = [None] * 3
     for i in range(3):
         grille[i] = [""] * 3
     return grille
 
+# pour choisir le nom du joueru
 def nom_joueur():
     nom = input(f"Entrez le nom du joueur :")
     print("")
     return nom
 
+# impose de choisir un symbole : soit "X", soit "O"
 def choix_symbole():
     print("choisissez un symbole : 1 pour X, 2 pour O")
     symboles = ["X", "O"]
